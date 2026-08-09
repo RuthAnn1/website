c22baa3af8210c0d8d5f0a7b125aeb19ed726849
Ruth Ann <129031001+RuthAnn1@users.noreply.github.com>
2026-08-09 16:54:30 -0500

    added files

---
+++ Portfolio/FinalProject/Recipie.js
@@ -0,0 +1,97 @@
+let recipie =[{
+	title : "Egg and Cheese Olmlet",
+	serving_Size: "1",
+	Ingreadient_list : [3+ " eggs", .25 +"cups of Cheese",1 +"tbspbutter"],
+	instructions:["add the eggs to a small bowl and with a fork whisk the eggs together", "put the fry pan on med heat add the 1 TBSP of butter the the pan", "once the butter is melted add the eggs and leave on the med heat", "once the eggs are partially cooked add  the .25 cup of cheese"] 
+
+},
+{
+	title:"lesagna",
+	serving_Size:1,
+	Ingreadient_list : [1+ "Cup of Flour ", 2 +"cups of milk", 2 +"TBSP butter", 1+"cup chocolate chips"],
+	instructions:["in one small bowl add the flour and chocolate chips", "the next small bowl add the melted butter and milk", "whisk the wet ingreadients together","add the dry ingreadients to the wet ingredients", "heat a pan on med heat", "add butter and fry quater size pancakes"]
+},
+{
+	title : "Waffels",
+	serving_Size: "2",
+	Ingreadient_list:["2 large eggs","2 cups all-purpose flour","1 cup milk"," .25 cup vegetable oil","1 tablespoon white sugar","4 teaspoons baking powder"," .25 teaspoon salt", ".25 teaspoon vanilla extract", "nonstick cooking spray"],
+	instructions:["Preheat a waffle iron according to manufacturer's instructions.","Whisk eggs in a large bowl until light and fluffy. Add flour, milk, and vegetable oil and mix to combine. Whisk in sugar, then mix in baking powder, salt, and vanilla just until smooth, being careful not to overmix.","Spray the preheated waffle iron with nonstick spray. Pour batter onto the hot waffle iron and cook until golden brown and the iron stops steaming, 3 to 5 minutes."]
+}
+
+]
+let luncRecipies =[{
+	title:"Grilled Ham and cheese sandwich",
+	serving_Size: 1,
+	Ingreadient_list :["4 tablespoons unsalted butter", "8 slices of bakery Pullman bread",".5 cup freshly grated Parmigiano-Reggiano cheese",".5 pound sliced Swiss cheese, preferably Gruyère","8 ounces thinly sliced ham","Dill pickle slices (optional)","2 tablespoons Dijon mustard",".25 cup apricot preserves"],
+	instructions:["Butter each slice of bread and sprinkle with Parmigiano", "pressing to help it adhere. Invert the slices onto a work surface", "cheese side down. Top four of the slices with the Swiss cheese, ham, and pickles, if using. Mix the mustard and preserves and spread on the remaining 4 slices of bread.", "Close the sandwiches and griddle over moderate heat, turning, until golden-crisp on the outside and melted inside, about 3 minutes.", "Cut in half and serve right away."]	
+	
+
+},
+{
+	title:"Veggie Quesadilla",
+	serving_Size: 1,
+	Ingreadient_list :["Large flour tortillas","Grated cheese such as mild or sharp cheddar","Olive oi"],
+	instructions:["Heat a large skillet (cast iron works great) on medium high heat.", "Add a small amount of oil (about 1/2 teaspoon) and spread it around the bottom of the pan with a spatula (you could use butter as well)","Take one large flour tortilla and place it in the pan. Flip the tortilla over a few times, 10 seconds between flips. Air pockets should begin to form within the tortilla.","When pockets of air begin to form, take a handful of grated cheese, sprinkle over the top of the tortilla, making sure that the cheese does not land on the pan itself.", "Add whatever additional ingredients you choose - green onion, sliced mushrooms, olives, tomatoes, etc. If you would like your quesadilla to be a chicken quesadilla, add some diced cooked chicken. Take care not to layer on the ingredients too thickly - this is a quesadilla, not a quiche!","Reduce the heat to low and cover the pan. The pan should be hot enough by now to have plenty of residual heat to melt the cheese and brown the tortilla. If the quesadilla begins to smoke too much, remove from the heat.", "After a minute, check to see if the cheese is melted. If not, return the cover and keep checking every minute until the cheese is melted.", "When the cheese is sufficiently melted, use a spatula to lift up one side of the quesadilla and flip over the other side, as if you were making an omelette.", "The tortilla should by now be browned slightly. If it is not browned, turn the heat up to high and flip the quesadilla over every 10 seconds or so until it gets browned.","To make the lettuce to accompany the quesadilla, thinly slice some iceberg lettuce. Sprinkle some cider vinegar on it and some salt.Serve with the lettuce, salsa, sour cream, and guacamole."]
+	
+},
+{
+	title:"Chilli",
+	serving_Size: 1,
+	Ingreadient_list :["2 pounds lean ground beef","1 onion diced","1 jalapeno seeded and finely diced",
+		"4 cloves garlic minced",
+		"2.5 tablespoons chili powder divided (or to taste)",
+		"1 teaspoon cumin",
+		"1 green bell pepper seeded and diced",
+		"14 ½ ounces crushed tomatoes canned",
+		"19 ounces kidney beans canned, drained & rinsed",
+		"14.25 ounces diced tomatoes with juice",
+		"1.24 cups beef broth",
+		"1 cup beer",
+		"1 tablespoon tomato paste",
+		"1 tablespoon brown sugar optional",
+		"salt and pepper to taste"],
+	instructions:["Combine ground beef and 1 ½ tablespoons chili powder.",
+		"In a large pot, brown ground beef, onion, jalapeno, and garlic. Drain any fat.",
+		"Add in remaining ingredients and bring to a boil. Reduce heat and simmer uncovered 45-60 minutes or until chili has reached desired thickness.",
+		"Top with cheddar cheese, green onions, cilantro or other favorite toppings."]
+}
+
+];
+let supperRecipies =[{
+	title:"Lesagna",
+	serving_Size: 6,
+	Ingreadient_list :["2 teaspoons extra virgin olive oil","1 pound ground beef chuck",".5 medium onion, diced (about 3/4 cup)",
+		".5 large bell pepper (green, red, or yellow), diced (about 3/4 cup)","2 cloves garlic, minced","1 (28-ounce)can good-quality tomato sauce","3 ounces tomato paste (half a 6-ounce can)",
+		"1 (14 ounce) can crushed tomatoes","2 tablespoons chopped fresh oregano, or 2 teaspoons dried oregano","1/4 cup chopped fresh parsley (preferably flat leaf), packed","1 tablespoon Italian seasoning",
+		"1 pinch garlic powder and/or garlic salt","1 tablespoon red or white wine vinegar",
+		"1 tablespoon to 1/4 cup sugar (to taste, optional)", "Salt"],
+	instructions:["Put a large pot of salted water (1 tablespoon of salt for every 2 quarts of water) on the stovetop on high heat. It can take a while for a large pot of water to come to a boil (this will be your pasta water), so prepare the sauce in the next steps while the water is heating.","In a large skillet heat 2 teaspoons of olive oil on medium-high heat. Add the ground beef and cook until it is lightly browned on all sides. Remove the beef with a slotted spoon to a bowl. Drain off all but a tablespoon of fat.","Add the diced bell pepper and onions to the skillet (in the photo we are using yellow bell pepper and red onions).", "Cook for 4 to 5 minutes, until the onions are translucent and the peppers softened. Add the minced garlic and cook half a minute more. Return the browned ground beef to the pan. Stir to combine, reduce the heat to low, and cook for another 5 minutes.",
+	"Transfer the beef mixture to a medium-sized (3- to 4-quart) pot. Add the crushed tomatoes, tomato sauce, and tomato paste to the pot.",
+	"Add the parsley, oregano, and Italian seasonings, adjusting the amounts to taste. Sprinkle with garlic powder and/or garlic salt, to taste. Sprinkle with red or white wine vinegar. Stir in sugar, a tablespoon at a time, tasting after each addition, to taste. (The amount of sugar needed will vary, depending on how acidic the tomatoes are that you are using.) Add salt to taste, and note that you will later be adding Parmesan, which is salty. Bring the sauce to a simmer and then lower the heat to maintain a low simmer. Cook for 15 to 45 minutes, stirring often. Scrape the bottom of the pot every so often so nothing sticks to the bottom and scorches. Remove from heat.","By now the salted water you started heating in step one should be boiling. Add the dry lasagna noodles and cook them to al dente, per package directions. (Note noodles may be cooked in advance.) Stir often to prevent from sticking. Make sure that water remains at a full rolling boil during the entire cooking to prevent noodles from sticking. When ready, drain in a colander and rinse with cool water, gently separating any noodles that may be sticking together. Spread a little olive oil on a large rimmed baking sheet, and lay out the cooked noodles on this sheet, turning them over so that they get coated with a little of the olive oil. This will prevent them from sticking together.",
+	"Preheat the oven to 375°F.","In a 9x13-inch casserole or lasagna dish, ladle a cup of sauce and spread it over the bottom of the dish. Arrange one layer of lasagna noodles lengthwise (about 3 long noodles, the edges may overlap, depending on your pan) over the sauce. Ladle a third of the remaining sauce over the noodles. Sprinkle a layer of a third of the grated mozzarella on top of the lasagna sauce. Add half of the ricotta cheese, by placing cheese dollops every couple of inches. Sprinkle half the grated parmesan cheese evenly over the top of the ricotta cheese. Apply the second layer of noodles and top it with half of the remaining sauce. Add half of the remaining mozzarella, the remaining ricotta cheese, and another the remaining Parmesan. Finish with another layer of noodles. Spread the remaining sauce over the top layer of noodles and sprinkle with the remaining mozzarella cheese.",
+	"Cover the lasagna pan with aluminum foil, tented slightly so it doesn't touch the noodles or sauce). Bake at 375°F for 45 minutes. Uncover in the last 10 minutes if you'd like more of a crusty top or edges.",
+	"Allow the lasagna to cool for at least 15 before serving. Leftovers will keep for about 5 days. You can reheat it in a conventional oven or microwave. Leave the aluminum tent on for storage. (Try to keep the aluminum foil from touching the sauce.)"],
+},
+{
+	title:"Spaghetti",
+	serving_Size: 5,
+	Ingreadient_list :["1 pound ground beef","1 medium onion, chopped","4 cloves garlic, minced","1 small green bell pepper, diced","1 (28 ounce) can diced tomatoes","1 (16 ounce) can tomato sauce","1 (6 ounce) can tomato paste","2 teaspoons dried oregano","2 teaspoons dried basil","1 teaspoon salt",".5 teaspoon ground black pepper"],
+	instructions:["Gather all ingredients.","Combine ground beef, onion, garlic, and green pepper in a large saucepan over medium-high heat. Cook and stir until meat is browned and crumbly and vegetables are tender, 5 to 7 minutes. Drain grease.","Stir diced tomatoes, tomato sauce, and tomato paste into the pan. Season with oregano, basil, salt, and pepper. Simmer spaghetti sauce for 1 hour, stirring occasionally.",
+	"Serve hot and enjoy!"],	
+},
+{
+	title:"Steak",
+	serving_Size: 4,
+	Ingreadient_list :["4 1 1/4-to-1 1/2-inch-thick boneless rib-eye or New York strip steaks (about 12 ounces each)", "or filets mignons (8 to 10 ounces each), trimmed","2 tablespoons canola or extra-virgin olive oil","Kosher salt and freshly ground pepper"],
+	instructions:["About 20 minutes before grilling, remove the steaks from the refrigerator and let sit, covered, at room temperature.","Heat your grill to high. Brush the steaks on both sides with oil and season liberally with salt and pepper. Place the steaks on the grill and cook until golden brown and slightly charred, 4 to 5 minutes. Turn the steaks over and continue to grill 3 to 5 minutes for medium-rare (an internal temperature of 135 degrees F), 5 to 7 minutes for medium (140 degrees F) or 8 to 10 minutes for medium-well (150 degrees F).","Transfer the steaks to a cutting board or platter, tent loosely with foil and let rest 5 minutes before slicing"],
+}
+
+
+
+]
+
+let recipies = [JSON.stringify(recipie)];
+let luncRecipie = [JSON.stringify(luncRecipies)];
+let supperRecipie = [JSON.stringify(supperRecipies)];
+
+
